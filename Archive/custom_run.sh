b15c1a303863a040ac3137ac30a515315fb4e380
jshaw35 <jonah.k.shaw@gmail.com>
2019-12-05 15:06:03 +0100

    legacy updates, not useful rn

--- Archive/custom_run.sh
+++ Archive/custom_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # ^specify bash as interpreter
 
-# Copied from slf_ibkt.sh by Jonah Shaw 19/11/05
+# Copied from slf_only.sh by Jonah Shaw 19/11/05
 # Testing bash scripts offline to learn functionality
 
 ############
@@ -21,10 +21,9 @@ function ponyfyer() {
 # SET INPUT ARGS
 ############
 
-args=("$@")
-CASENAME=${args[0]}  # uniquecasename, maybe add a timestamp in the python script
-wbf=${args[1]}          # wbf multiplier
-inp=${args[2]}          # inp multiplier
+CASENAME='lat_ind_inp1_2'  # uniquecasename, maybe add a timestamp in the python script
+wbf=1.0          # wbf multiplier
+inp=1.0          # inp multiplier
 
 #echo ${args[0]} ${args[1]} ${args[2]}
 
@@ -34,7 +33,7 @@ inp=${args[2]}          # inp multiplier
 
 models=("noresm-dev" "cesm" "noresm-dev-10072019")
 compsets=("NF2000climo" "N1850OCBDRDDMS")
-resolutions=("f19_tn14")
+resolutions=("f19_tn14" "f10_f10_mg37")
 machines=('fram')
 projects=('nn9600k')
 
@@ -46,8 +45,8 @@ CASEROOT=/cluster/home/jonahks/p/jonahks/cases
 
 # Where FORTRAN files contains microphysics modifications are stored
 # May require future subdirectories
-ModSource=/cluster/home/jonahks/sourcemods/wbf_slf
-#ModSource=/cluster/home/jonahks/git_repos/all_cloud_mods
+#ModSource=/cluster/home/jonahks/sourcemods/wbf_slf
+ModSource=/cluster/home/jonahks/git_repos/noresm2_mpc/SourceMods
 
 # Case name, unique, could be configured as an input arg:
 # CASENAME=NF2000climo_reshere_initialtest
@@ -59,6 +58,7 @@ MACH=${machines[0]}
 PROJECT=${projects[0]}
 MISC=--run-unsupported
 
+NUMNODES=-4 # How many nodes each component should run on
 # COMPSET=NF2000climo
 # RES=f19_tn14
 # MACH=fram
@@ -73,8 +73,6 @@ echo ${CASEROOT}/${CASENAME} ${COMPSET} ${RES} ${MACH} ${PROJECT} $MISC
 cd ${ModelRoot} # Move to appropriate directory
 #pwd
 
-#exit 1
-
 # Create env_*.xml files
 ./create_newcase --case ${CASEROOT}/${CASENAME} \
                  --compset ${COMPSET} \
@@ -83,20 +81,20 @@ cd ${ModelRoot} # Move to appropriate directory
                  --project ${PROJECT} \
                  $MISC
 
-exit 1
-
 cd ${CASEROOT}/${CASENAME} # Move to the case's dir
 
 # Set run time and restart variables within env_run.xml
 #./xmlchange --file=env_run.xml RESUBMIT=3
 ./xmlchange --file=env_run.xml STOP_OPTION=nmonth
 ./xmlchange --file=env_run.xml STOP_N=1
-# ./xmlchange --file=env_batch.xml JOB_WALLCLOCK_TIME=00:50:00 --subgroup case.run
-#./xmlchange --file=env_run.xml REST_OPTION=nyears
+./xmlchange --file=env_batch.xml JOB_WALLCLOCK_TIME=00:59:00 --subgroup case.run
+# ./xmlchange --file=env_run.xml REST_OPTION=nyears
 #./xmlchange --file=env_run.xml REST_N=5
 #./xmlchange -file env_build.xml -id CAM_CONFIG_OPTS -val '-phys cam5'
 
-# Do I need to modify the env_mach_pres.xml file here? How do I do that?
+# Modify the env_mach_pres.xml file here. If NUMTASKS is -4, it should get off the queue faster
+./xmlchange --file=env_mach_pes.xml -id NTASKS --val ${NUMNODES}
+./xmlchange --file=env_mach_pes.xml -id NTASKS_ESP --val 1
 
 # Move modified WBF process into SourceMods dir:
 cp ${ModSource}/micro_mg_cam.F90 /${CASEROOT}/${CASENAME}/SourceMods/src.cam
@@ -109,14 +107,13 @@ cp ${ModSource}/micro_mg2_0.F90 /${CASEROOT}/${CASENAME}/SourceMods/src.cam
 cp ${ModSource}/hetfrz_classnuc_cam.F90 /${CASEROOT}/${CASENAME}/SourceMods/src.cam
 
 # Now use ponyfyer to set the values within the sourcemod files. Ex:
-# ponyfyer 'wahooey' 'BUTT' 'pretend_fortran.F90'
 mg2_path=/${CASEROOT}/${CASENAME}/SourceMods/src.cam/micro_mg2_0.F90
-#ponyfyer 'wbf_tag' ${wbf} ${mg2_path}
-ponyfyer 'wbf_tag = 1.' ${wbf} ${mgcam_path}
-
-
 nuc_i_path=/${CASEROOT}/${CASENAME}/SourceMods/src.cam/hetfrz_classnuc_cam.F90
-ponyfyer 'inp_tag = 1.' ${inp} ${nuc_i_path}
+
+ponyfyer 'wbf_tag = 1.' "wbf_tag = ${wbf}" ${mg2_path}
+ponyfyer 'inp_tag = 1.' "inp_tag = ${inp}" ${nuc_i_path}
+
+#echo ${mg2_path} ${inp2} ${nuc_i_path}
 
 exit 1
 # Will need to set these values in some manner now
@@ -127,22 +124,23 @@ exit 1
 # Will need to modify the nl files appropriately here to choose output
 # CAM adjustments, I don't entirely understand the syntax here, but all the formatting after the first line is totally preserved:
 # list variables to add to first history file here
+#&aerosol_nl  # Not sure what this is.
+# , 'SLFXCLD_ISOTM', 'SADLIQXCLD_ISOTM', 'SADICEXCLD_ISOTM', 'BERGOXCLD_ISOTM',
+# 'BERGSOXCLD_ISOTM', 'CLD_ISOTM', 'CLDTAU', 'CLD_SLF', 'CLD_ISOTM_SLF',
 cat <<TXT2 >> user_nl_cam
-&aerosol_nl  # Not sure what this is.
-fincl1 = 'BERGO', 'BERGSO', 'SLFXCLD_ISOTM', 'SADLIQXCLD_ISOTM', 'SADICEXCLD_ISOTM', 'BERGOXCLD_ISOTM',
-         'BERGSOXCLD_ISOTM', 'CLD_ISOTM', 'CLDTAU', 'CLD_SLF', 'CLD_ISOTM_SLF', 'MNUCCTO', 'MNUCCRO', 'MNUCCCO', 'MNUCCDOhet', 'MNUCCRO'
-         'DSTFREZIMM', 'DSTFREZCNT', 'DSTFREZDEP', 'BCFREZIMM', 'BCFREZCNT', 'BCFREZDEP',
+fincl1 = 'BERGO', 'BERGSO', 'MNUCCTO', 'MNUCCRO', 'MNUCCCO', 'MNUCCDOhet', 'MNUCCDO', DSTFREZIMM,
+         'DSTFREZCNT', 'DSTFREZDEP', 'BCFREZIMM', 'BCFREZCNT', 'BCFREZDEP',
          'NUMICE10s', 'NUMICE10sDST', 'NUMICE10sBC',
          'dc_num', 'dst1_num', 'dst3_num', 'bc_c1_num', 'dst_c1_num', 'dst_c3_num'
-
-use_hetfrz_classnuc = .true.
-
 TXT2
+#nhtfrq(1) = 0
 
-exit 1
+# exit 1
 
 # build, create *_in files under run/
 ./case.build
+
+exit 1
 
 # Submit the case
 ./case.submit
